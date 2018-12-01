ae0081070eb99068905fff28ed91568940a286de
invntiv <mack.joshuaj@gmail.com>
2018-12-01 16:48:41 -0600

    Initial commit

---
+++ app/src/main/java/team383/edu/panthertheatres/LocationsFragment.java
@@ -0,0 +1,82 @@
+package team383.edu.panthertheatres;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.util.Log;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import static android.content.ContentValues.TAG;
+
+
+public class LocationsFragment extends Fragment implements OnMapReadyCallback {
+
+    private View rootView;
+    GoogleMap myMap;
+    MapView mMapView;
+
+    @Nullable
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try {
+            rootView = inflater.inflate(R.layout.fragment_locations, container, false);
+            MapsInitializer.initialize(this.getActivity());
+            mMapView = (MapView) rootView.findViewById(R.id.map);
+            mMapView.onCreate(savedInstanceState);
+            mMapView.getMapAsync(this);
+        }
+        catch (InflateException e){
+            Log.e(TAG, "Inflate exception");
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        {
+            //TODO
+        }
+
+    }
+}
---
+++ app/src/main/java/team383/edu/panthertheatres/MoviesFragment.java
@@ -0,0 +1,18 @@
+package team383.edu.panthertheatres;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MoviesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+}
